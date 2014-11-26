e109a0fbea22abf892e9c20fb0945719ab2e38d8
wei bai <baiwei0427@sina.com>
2014-11-27 01:41:54 +0800

    Upload Trinity kernel module

---
+++ Trinity/rx.h
@@ -0,0 +1,165 @@
+#ifndef	RX_H
+#define	RX_H
+
+#include <linux/list.h>
+#include <linux/time.h>  
+
+//Define the structure of RX statistics
+struct rx_stats
+{
+	unsigned long rx_bytes;
+	unsigned long rx_ecn_bytes;
+};
+
+//Define the structure of per VM-to-VM pair RX context
+struct pair_rx_context 
+{
+	unsigned int local_ip;			
+	unsigned int remote_ip;			
+	//Statistic information
+	struct rx_stats stats;
+	//Bandwidth guarantee rate (Mbps)
+	unsigned int rate;
+	ktime_t last_update_time;
+	//Structure of link list
+	struct list_head list;
+};
+
+//Define the structure of per endpoint (VM) RX context
+struct endpoint_rx_context
+{
+	unsigned int local_ip;
+	//The total guarantee bandwidth for the endpoint
+	unsigned int guarantee_bw;
+	//The head point of link list for VM-to-VM pair RX context 
+	struct list_head pair_list;
+	//Structure of link list
+	struct list_head list;
+	//The  number of VM-to-VM pairs for this endpoint
+	unsigned int pair_num; 
+};
+
+//Define the structure for (per NIC/physical server) RX context
+struct rx_context
+{
+	//The head point of link list for endpoint RX context
+	struct list_head endpoint_list;
+	//The number of endpoints for this physical server
+	unsigned int endpoint_num;
+};
+
+static void print_pair_rx_context(struct pair_rx_context* ptr)
+{
+	char local_ip[16]={0};           
+	char remote_ip[16]={0};      
+	snprintf(local_ip, 16, "%pI4", &(ptr->local_ip));
+	snprintf(remote_ip, 16, "%pI4", &(ptr->remote_ip));
+	printk(KERN_INFO "%s->%s, %lu/%lu, pair bandwidth guarantee is %u Mbps\n",local_ip,remote_ip,ptr->stats.rx_ecn_bytes,ptr->stats.rx_bytes,ptr->rate);
+}
+
+static void print_endpoint_rx_context(struct endpoint_rx_context* ptr)
+{
+	char local_ip[16]={0};      
+	snprintf(local_ip, 16, "%pI4", &(ptr->local_ip));
+	printk(KERN_INFO "%s, endpoint bandwidth guarantee is %u Mbps\n",local_ip,ptr->guarantee_bw);
+}
+
+static void print_rx_context(struct rx_context* ptr)
+{
+	struct endpoint_rx_context* endpoint_ptr=NULL; 
+	struct pair_rx_context* pair_ptr=NULL;
+	
+	list_for_each_entry(endpoint_ptr,&(ptr->endpoint_list),list)
+	{
+		print_endpoint_rx_context(endpoint_ptr);
+		list_for_each_entry(pair_ptr,&(endpoint_ptr->pair_list),list)
+		{
+			print_pair_rx_context(pair_ptr);
+		}
+	}	
+}
+
+//Initialize RX context
+static void Init_rx_context(struct rx_context* ptr)
+{
+	ptr->endpoint_num=0;
+	INIT_LIST_HEAD(&(ptr->endpoint_list));
+}
+
+//Initialize endpoint RX context
+static void Init_endpoint_rx_context(struct endpoint_rx_context* ptr, unsigned int ip, unsigned int bw)
+{
+	ptr->pair_num=0;
+	ptr->local_ip=ip;
+	ptr->guarantee_bw=bw;
+	INIT_LIST_HEAD(&(ptr->pair_list));
+	INIT_LIST_HEAD(&(ptr->list));
+}
+
+//Initialize pair RX context
+static void Init_pair_rx_context(struct pair_rx_context* ptr, unsigned int local_ip, unsigned int remote_ip, unsigned int bw)
+{
+	ptr->local_ip=local_ip;
+	ptr->remote_ip=remote_ip;
+	ptr->rate=bw;
+	ptr->stats.rx_bytes=0;
+	ptr->stats.rx_ecn_bytes=0;
+	//The last update time is set to current time
+	ptr->last_update_time=ktime_get();
+	INIT_LIST_HEAD(&(ptr->list));
+}
+
+//Insert a new pair RX context to an endpoint RX context
+static void Insert_pair_endpoint(struct pair_rx_context* pair_ptr, struct endpoint_rx_context* endpoint_ptr)
+{
+	list_add_tail(&(pair_ptr->list),&(endpoint_ptr->pair_list));
+	endpoint_ptr->pair_num++;
+}
+
+//Insert a new pair RX context to a RX context
+static void Insert_pair(struct pair_rx_context* pair_ptr, struct rx_context* ptr)
+{
+	struct endpoint_rx_context* endpoint_ptr=NULL; 
+	list_for_each_entry(endpoint_ptr,&(ptr->endpoint_list),list)
+	{
+		//If the local_ip is matched
+		if(endpoint_ptr->local_ip==pair_ptr->local_ip)
+		{
+			//Insert the pair RX context to corresponding endpoint RX context
+			Insert_pair_endpoint(pair_ptr, endpoint_ptr);
+			return;
+		}
+	}
+	printk(KERN_INFO "Can not find matching endpoint RX information entry\n");
+} 
+
+//Insert a new endpoint TX context to a RX context
+static void Insert_endpoint(struct endpoint_rx_context* endpoint_ptr, struct rx_context* ptr)
+{
+	list_add_tail(&(endpoint_ptr->list),&(ptr->endpoint_list));
+	ptr->endpoint_num++;	
+}
+
+//Clear all endpoint or pair RX information entries 
+static void Empty_rx_context(struct rx_context* ptr)
+{
+	struct endpoint_rx_context* endpoint_ptr=NULL; 
+	struct endpoint_rx_context* endpoint_next=NULL; 
+	struct pair_rx_context* pair_ptr=NULL; 
+	struct pair_rx_context* pair_next=NULL; 
+	
+	list_for_each_entry_safe(endpoint_ptr, endpoint_next, &(ptr->endpoint_list), list)
+    {
+		print_endpoint_rx_context(endpoint_ptr);
+		list_for_each_entry_safe(pair_ptr, pair_next, &(endpoint_ptr->pair_list), list)
+		{
+			print_pair_rx_context(pair_ptr);	
+			list_del(&(pair_ptr->list));
+			kfree(pair_ptr);
+		}
+		list_del(&(endpoint_ptr->list));
+		kfree(endpoint_ptr);
+    }	
+}
+
+#endif
---
+++ Trinity/trinity.c
@@ -0,0 +1,203 @@
+#include <linux/module.h> 
+#include <linux/kernel.h> 
+#include <linux/init.h>
+#include <linux/types.h>
+#include <linux/netfilter.h>
+#include <linux/skbuff.h>
+#include <linux/ip.h>
+#include <linux/netdevice.h>
+#include <linux/if_ether.h>
+#include <linux/if_packet.h>
+#include <linux/inet.h>
+#include <net/tcp.h>
+#include <net/udp.h>
+#include <net/icmp.h>
+#include <linux/netfilter_ipv4.h>
+#include <linux/string.h>
+#include <linux/time.h>  
+#include <linux/fs.h>
+#include <linux/random.h>
+#include <linux/errno.h>
+#include <linux/timer.h>
+#include <linux/vmalloc.h>
+#include <linux/list.h>
+
+#include "rx.h"
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("BAI Wei wbaiab@cse.ust.hk");
+MODULE_VERSION("1.0");
+MODULE_DESCRIPTION("Kernel module of  Trinity");
+
+char *param_dev=NULL;
+MODULE_PARM_DESC(param_dev, "Interface to operate Trinity");
+module_param(param_dev, charp, 0);
+
+//Hook for outgoing packets at LOCAL_OUT 
+static struct nf_hook_ops nfho_outgoing;
+//Hook for outgoing packets at LOCAL_IN
+static struct nf_hook_ops nfho_incoming;
+//RX context pointer
+static struct rx_context* rxPtr;
+
+//POSTROUTING for outgoing packets
+static unsigned int hook_func_out(unsigned int hooknum, struct sk_buff *skb, const struct net_device *in, const struct net_device *out, int (*okfn)(struct sk_buff *))
+{
+	if(!out)
+		return NF_ACCEPT;
+        
+	if(strcmp(out->name,param_dev)!=0)
+		return NF_ACCEPT;
+	
+	return NF_ACCEPT;
+}
+
+//PREROUTING for incoming packets
+static unsigned int hook_func_in(unsigned int hooknum, struct sk_buff *skb, const struct net_device *in, const struct net_device *out, int (*okfn)(struct sk_buff *))
+{
+	if(!in)
+		return NF_ACCEPT;
+    
+	if(strcmp(in->name,param_dev)!=0)
+		return NF_ACCEPT;
+	
+	return NF_ACCEPT;	
+}
+
+int init_module()
+{
+	struct endpoint_rx_context* endpointPtr=NULL;
+	struct pair_rx_context* pairPtr=NULL;  
+	int i=0;
+	
+	//Get interface
+    if(param_dev==NULL) 
+    {
+        printk(KERN_INFO "Trinity: not specify network interface.\n");
+        param_dev = "eth1\0";
+	}
+	// trim 
+	for(i = 0; i < 32 && param_dev[i] != '\0'; i++) 
+	{
+		if(param_dev[i] == '\n') 
+		{
+			param_dev[i] = '\0';
+			break;
+		}
+	}
+	
+	//Initialize RX context information
+	rxPtr=kmalloc(sizeof(struct rx_context), GFP_KERNEL);
+	Init_rx_context(rxPtr);
+	
+	//NF_LOCAL_IN Hook
+	nfho_incoming.hook = hook_func_in;							//function to call when conditions below met
+	nfho_incoming.hooknum =  NF_INET_LOCAL_IN;		//called in NF_IP_LOCAL_IN
+	nfho_incoming.pf = PF_INET;											//IPv4 packets
+	nfho_incoming.priority = NF_IP_PRI_FIRST;				//set to highest priority over all other hook functions
+	nf_register_hook(&nfho_incoming);								//register hook*/
+	
+	//NF_LOCAL_OUT Hook
+	nfho_outgoing.hook = hook_func_out;						//function to call when conditions below met
+	nfho_outgoing.hooknum =  NF_INET_LOCAL_OUT;	//called in NF_IP_LOCAL_OUT
+	nfho_outgoing.pf = PF_INET;											//IPv4 packets
+	nfho_outgoing.priority = NF_IP_PRI_FIRST;				//set to highest priority over all other hook functions
+	nf_register_hook(&nfho_outgoing);								//register hook	
+	
+	//Test code
+	
+	printk(KERN_INFO "Start Trinity kernel module\n");
+	return 0;
+}
+
+void cleanup_module()
+{
+	//Unregister two hooks
+	nf_unregister_hook(&nfho_outgoing);  
+	nf_unregister_hook(&nfho_incoming);	
+	
+	Empty_rx_context(rxPtr);	
+	kfree(rxPtr);
+	printk(KERN_INFO "Stop Trinity kernel module\n");
+	
+}
+/*int init_module()
+{
+	int i,j;
+	struct endpoint_rx_context* endpointPtr=NULL;
+	struct pair_rx_context* pairPtr=NULL;  
+	
+	printk(KERN_INFO "Start Trinity kernel module\n");
+	rxPtr=kmalloc(sizeof(struct rx_context), GFP_KERNEL);
+	Init_rx_context(rxPtr);
+	for(i=1;i<5;i++)
+	{
+		endpointPtr=kmalloc(sizeof(struct endpoint_rx_context), GFP_KERNEL);	
+		Init_endpoint_rx_context(endpointPtr,i*256*256*256+168*256+192,400);
+		Insert_endpoint(endpointPtr,rxPtr);
+		
+		for(j=1;j<5;j++)
+		{
+			pairPtr=kmalloc(sizeof(struct pair_rx_context), GFP_KERNEL);	
+			Init_pair_rx_context(pairPtr,i*256*256*256+168*256+192,j*256*256*256+i*256*256+168*256+192,100);
+			Insert_pair(pairPtr,rxPtr);
+		}
+	}
+	
+	print_rx_context(rxPtr);
+	return 0;
+}
+
+void cleanup_module()
+{
+	Empty_rx_context(rxPtr);	
+	kfree(rxPtr);
+	printk(KERN_INFO "Remove Trinity kernel module\n");
+}*/
+
+/*static struct list_head pair_list;
+	
+//Called when module loaded using 'insmod'¡¡
+int init_module()
+{
+	int i=0;
+	struct pair_rx_context* ptr=NULL; 
+	
+	INIT_LIST_HEAD(&pair_list);
+	//Allocate four pair_rx_context from kernel
+	for(i=1;i<5;i++)
+	{
+		ptr=kmalloc(sizeof(struct pair_rx_context), GFP_KERNEL);
+		ptr->local_ip=i*256*256*256+168*256+192;
+		ptr->remote_ip=i*256*256*256+256*256+168*256+192;
+		ptr->rate=100;
+		ptr->stats.rx_bytes=i;
+		ptr->stats.rx_ecn_bytes=i;
+		INIT_LIST_HEAD(&(ptr->list));
+		//Add this structure to the tail of the linked list
+		list_add_tail(&(ptr->list),&pair_list);
+	}
+	printk(KERN_INFO "Display the list:\n");
+	list_for_each_entry(ptr,&pair_list,list)
+	{
+		print_pair_rx_context(ptr);
+	}
+	printk(KERN_INFO "Display done\n");
+	return 0;
+}
+
+//Called when module unloaded using 'rmmod'
+void cleanup_module()
+{
+	struct pair_rx_context* ptr=NULL; 
+	struct pair_rx_context* next=NULL; 
+	printk(KERN_INFO "Remove start\n");	
+	list_for_each_entry_safe(ptr, next, &pair_list, list)
+    {
+		print_pair_rx_context(ptr);
+        list_del(&ptr->list);
+        kfree(ptr);
+    }	
+	printk(KERN_INFO "Remove done\n");	
+}*/
+
